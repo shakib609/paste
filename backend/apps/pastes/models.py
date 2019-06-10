80be7f04a69be5d072918ab70a6854a4609d1158
Shakib Hossain <shakib609@gmail.com>
2019-06-10 15:56:05 +0600

    Add graphene_django to INSTALLED_APPS and write tests for the pastes.Paste model

--- backend/apps/pastes/models.py
+++ backend/apps/pastes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
 
 
 class Folder(models.Model):
@@ -10,15 +11,19 @@ class Folder(models.Model):
         related_name="folders",
     )
 
+    class Meta:
+        ordering = ["name"]
+        unique_together = [["name", "created_by"]]
+
     def __str__(self):
-        return f"{self.name} - f{self.created_by}"
+        return f"{self.name} - {self.created_by}"
 
 
 class Paste(models.Model):
     title = models.CharField(max_length=256, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    content = models.TextField()
+    content = models.TextField(default=None)
     public = models.BooleanField(default=True)
     folder = models.ForeignKey(
         Folder,
@@ -34,6 +39,16 @@ class Paste(models.Model):
         null=True,
         blank=True,
     )
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def save(self, *args, **kwargs):
+        if self.folder is not None and self.created_by is None:
+            raise IntegrityError(
+                'Paste can not be assigned a folder without created_by.')
+        else:
+            return super().save(*args, **kwargs)
 
     def __str__(self):
         if self.title:
--- backend/apps/pastes/tests.py
+++ backend/apps/pastes/tests.py
@@ -1,3 +1,64 @@
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Folder, Paste
+
+
+class PasteModelTestCase(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create(
+            username="test_user",
+            email="test@user.com",
+        )
+        self.folder = Folder.objects.create(
+            name="Test Folder",
+            created_by=self.user,
+        )
+
+    def test_paste_can_not_be_created_without_content(self):
+        """Pastes can not be created without content property"""
+        self.assertRaises(IntegrityError, Paste.objects.create)
+
+    def test_paste_can_be_created_with_only_content(self):
+        """Pastes can be created with only its contents"""
+        content = 'print("Hello World")'
+        self.assertEqual(
+            Paste.objects.create(content=content).content,
+            content,
+        )
+
+    def test_paste_can_be_created_by_any_user(self):
+        """Paste can be created by authenticated and unauthenticated users"""
+        content = "test_content"
+        # Anonymous User
+        Paste.objects.create(content=content)
+        # Authenticated User
+        Paste.objects.create(content=content, created_by=self.user)
+        self.assertEqual(Paste.objects.count(), 2)
+
+    def test_paste_can_belong_to_a_folder_if_created_by_is_specified(self):
+        """Paste can belong to a folder if created_by is specified"""
+        p = Paste.objects.create(
+            content="print('Hello World')",
+            folder=self.folder,
+            created_by=self.user,
+        )
+        self.assertEqual(p.folder, self.folder)
+        self.assertEqual(Paste.objects.count(), 1)
+
+    def test_paste_cannot_have_a_folder_if_created_by_is_not_specified(self):
+        """Paste cannot be assigned a folder if created_by is not specified"""
+        self.assertRaises(
+            IntegrityError,
+            Paste.objects.create,
+            content="print('hello world!')",
+            folder=self.folder,
+        )
+        self.assertEqual(Paste.objects.count(), 0)
+
+
+class FolderModelTestCase(TestCase):
+    # TODO: Write Tests for Folder model
+    pass
