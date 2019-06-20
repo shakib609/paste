832efa0e91bbdf8c292239fbcd88d1914bf1a7ce
Shakib Hossain <shakib609@gmail.com>
2019-06-21 02:51:07 +0600

    Update urls.py to use index as fallback view

--- backend/urls.py
+++ backend/urls.py
@@ -14,14 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 from .views import index
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('api/gql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('admin/', admin.site.urls),
+    path('api/gql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    re_path(r'.*', index, name='index'),
 ]
